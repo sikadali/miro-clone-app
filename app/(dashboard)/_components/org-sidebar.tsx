b536103b37e155362d19eb252bfd484d58f517bb
DALI <sika.dali@telecomnancy.net>
2024-08-24 04:17:12 +0200

    dashboard layout

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const OrgSideBar = () => {
+     return <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">ORG SIDEBAR</div>;
+};
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+import { UserButton } from "@clerk/nextjs";
+
+const DashboardPage = () => {
+     return <>Dashboard Root page</>;
+};
+
+export default DashboardPage;
