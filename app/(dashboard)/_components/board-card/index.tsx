711b8b6e116bb6d84b022e2e68503fbaffd7c04f
DALI <sika.dali@telecomnancy.net>
2024-08-25 18:03:42 +0200

    finish board card rendering

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -2,6 +2,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Overlay } from "./overlay";
+import { formatDistanceToNow } from "date-fns";
+import { useAuth } from "@clerk/clerk-react";
+import { Footer } from "./footer";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface BoardCardProps {
      id: string;
@@ -24,6 +28,11 @@ export const BoardCard = ({
      orgId,
      isFavorite,
 }: BoardCardProps) => {
+     const { userId } = useAuth();
+
+     const authorLabel = userId === authorId ? "You" : authorName;
+     const createdAtLabel = formatDistanceToNow(createdAt, { addSuffix: true });
+
      return (
           <Link href={`/board/${id}`}>
                <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
@@ -31,7 +40,23 @@ export const BoardCard = ({
                          <Image src={imageUrl} alt={title} fill className="object-fit" />
                          <Overlay />
                     </div>
+                    <Footer
+                         isFavorite={isFavorite}
+                         title={title}
+                         authorLabel={authorLabel}
+                         createdAtLabel={createdAtLabel}
+                         onClick={() => {}}
+                         disabled={false}
+                    />
                </div>
           </Link>
+     );
+};
+
+BoardCard.Skeleton = function BoardCardSkeleton() {
+     return (
+          <div className="aspect-[100/127] rounded-lg overflow-hidden">
+               <Skeleton className="w-full h-full" />
+          </div>
      );
 };
--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -7,6 +7,7 @@ import { EmptyBoards } from "./empty-boards";
 import { EmptyFavorites } from "./empty-favorites";
 import { EmptySearch } from "./empty-search";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
      orgId: string;
@@ -20,7 +21,19 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
      const data = useQuery(api.boards.get, { orgId });
 
      if (data === undefined) {
-          return <div>Loading...</div>;
+          return (
+               <div>
+                    <h2 className="text-3xl">{query.favorites ? "Favorite boards" : "Team boards"}</h2>
+
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                         <NewBoardButton orgId={orgId} disabled />
+                         <BoardCard.Skeleton />
+                         <BoardCard.Skeleton />
+                         <BoardCard.Skeleton />
+                         <BoardCard.Skeleton />
+                    </div>
+               </div>
+          );
      }
      if (!data?.length && query.search) {
           return <EmptySearch />;
@@ -35,6 +48,7 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
           <div>
                <h2 className="text-3xl">{query.favorites ? "Favorite boards" : "Team boards"}</h2>
                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                    <NewBoardButton orgId={orgId} />
                     {data?.map((board) => (
                          <BoardCard
                               key={board._id}
