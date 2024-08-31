39edbaf09eb61f9a72bc5e945d55287789e8ce13
DALI <sika.dali@telecomnancy.net>
2024-08-31 23:08:48 +0200

    favoriting functionality

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -1,5 +1,6 @@
 import { Star } from "lucide-react";
 import { cn } from "@/lib/utils";
+import React from "react";
 
 interface FooterProps {
      title: string;
@@ -11,6 +12,11 @@ interface FooterProps {
 }
 
 export const Footer = ({ title, authorLabel, createdAtLabel, isFavorite, onClick, disabled }: FooterProps) => {
+     const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+          event.stopPropagation();
+          event.preventDefault();
+          onClick();
+     };
      return (
           <div className="relative bg-white p-3">
                <p className="text-[13px] truncate max-w-[calc(100%-20px)]">{title}</p>
@@ -19,7 +25,7 @@ export const Footer = ({ title, authorLabel, createdAtLabel, isFavorite, onClick
                </p>
                <button
                     disabled={disabled}
-                    onClick={onClick}
+                    onClick={handleClick}
                     className={cn(
                          "opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",
                          disabled && "cursor-not-allowed opacity-75"
